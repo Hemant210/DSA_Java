dc2642ccfefe53b0a6f70fd84dbf83632546ea73
Hemant210 <hemantpatil.2103@gmail.com>
2025-06-25 11:48:53 +0530

    Add example Reverse string

--- Leetcode/Recursion_practice.java
+++ Leetcode/Recursion_practice.java
@@ -1,18 +1,37 @@
 package Leetcode;
 
 //Tower of Hanoi
+//Time complexity 
+// public class Recursion_practice {
+//     public static void print_hanoi(int n,String src,String help,String dest){
+//         if(n == 1){
+//             System.out.println("Trsnsfer disk :- " + n + " from " + src + " to " + dest);
+//             return;
+//         }
+//         print_hanoi(n-1, src, dest, help);
+//         System.out.println("Trsnsfer disk :- " + n + " from " + src + " to " + dest );
+//         print_hanoi(n-1, help, src, dest);
+//     }
+//     public static void main(String[] args) {
+//         int n = 3;
+//         print_hanoi(n, "S", "H", "D");
+//     }
+// }
+
+//Reverse string
+//Time complexity  = O(n)
 public class Recursion_practice {
-    public static void print_hanoi(int n,String src,String help,String dest){
-        if(n == 1){
-            System.out.println("Trsnsfer disk :- " + n + " from " + src + " to " + dest);
+    public static void print_rev(String s, int idx){
+        if(idx == 0){
+            System.out.println(s.charAt(idx));
             return;
         }
-        print_hanoi(n-1, src, dest, help);
-        System.out.println("Trsnsfer disk :- " + n + " from " + src + " to " + dest );
-        print_hanoi(n-1, help, src, dest);
+        System.out.print(s.charAt(idx));
+        print_rev(s, idx-1);
     }
-    public static void main(String[] args) {
-        int n = 3;
-        print_hanoi(n, "S", "H", "D");
-    }
+
+     public static void main(String[] args) {
+        String s = "abcd";
+        print_rev(s, s.length()-1);
+     }
 }
