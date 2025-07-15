e09a71a603e11c480fcc2a720b4644b617ccf5ce
Hemant210 <hemantpatil.2103@gmail.com>
2025-07-15 23:37:16 +0530

    Insert Node at the End of a Linked List - solution code

--- Practice/Linklist_practice.java
+++ Practice/Linklist_practice.java
@@ -102,7 +102,6 @@ package Practice;
 //     }
 // }
 
-
 // public class Linklist_practice {
 
 //     // public static Node reverseList(Node head) {
@@ -145,7 +144,7 @@ package Practice;
 
 //         System.out.print("Given Linked list:");
 //         printList(head);
-        
+
 //         head = reverList(head);
 
 //         System.out.print("\nReversed Linked List:");
@@ -154,86 +153,151 @@ package Practice;
 // }
 
 // Java program to delete a linked list node at a given
-// position
+// class Node {
+//     int data;
+//     Node next;
 
-// Node class to define a linked list node
+//     Node(int data) {
+//         this.data = data;
+//         this.next = null;
+//     }
+// }
+
+// public class Linklist_practice {
+//     // Function to delete a node at a given position
+//     public static Node deleteNode(Node head, int position) {
+//         Node temp = head;
+//         Node prev = null;
+
+//         // Base case if linked list is empty
+//         if (temp == null)
+//             return head;
+
+//         // Case 1: Head is to be deleted
+//         if (position == 1) {
+//             head = temp.next;
+//             return head;
+//         }
+
+//         // Case 2: Node to be deleted is in middle
+//         // Traverse till given position
+//         for (int i = 1; temp != null && i < position; i++) {
+//             prev = temp;
+//             temp = temp.next;
+//         }
+
+//         // If given position is found, delete node
+//         if (temp != null) {
+//             prev.next = temp.next;
+//         } else {
+//             System.out.println("Data not present");
+//         }
+
+//         return head;
+//     }
+
+//     // Function to print the linked list
+//     public static void printList(Node head) {
+//         while (head != null) {
+//             System.out.print(head.data + " -> ");
+//             head = head.next;
+//         }
+//         System.out.println("null");
+//     }
+
+//     // Driver code
+//     public static void main(String[] args) {
+//         // Creating a static linked list
+//         // 1 -> 2 -> 3 -> 4 -> 5 -> null
+//         Node head = new Node(1);
+//         head.next = new Node(2);
+//         head.next.next = new Node(3);
+//         head.next.next.next = new Node(4);
+//         head.next.next.next.next = new Node(5);
+
+//         // Print original list
+//         System.out.print("Original list: ");
+//         printList(head);
+
+//         // Deleting node at position 2
+//         int position = 2;
+//         head = deleteNode(head, position);
+
+//         // Print list after deletion
+//         System.out.print("List after deletion: ");
+//         printList(head);
+//     }
+// }
+
+//Insert Node at the End of a Linked List
 class Node {
     int data;
     Node next;
 
-    Node(int data)
-    {
-        this.data = data;
-        this.next = null;
+    // Constructor to initialize a new node with data
+    Node(int newData) {
+        data = newData;
+        next = null;
     }
 }
 
-public class Linklist_practice {
-    // Function to delete a node at a given position
-    public static Node deleteNode(Node head, int position)
-    {
-        Node temp = head;
-        Node prev = null;
+public class Linklist_practice {   
+    // Given the head of a list and an int, appends
+    // a new node at the end and returns the head.
+    public static Node append(Node head, int newData) {
+      
+        // Create a new node
+        Node newNode = new Node(newData);
 
-        // Base case if linked list is empty
-        if (temp == null)
-            return head;
-
-        // Case 1: Head is to be deleted
-        if (position == 1) {
-            head = temp.next;
-            return head;
+        // If the Linked List is empty, make the new 
+        // node as the head and return
+        if (head == null) {
+            return newNode;
         }
 
-        // Case 2: Node to be deleted is in middle
-        // Traverse till given position
-        for (int i = 1; temp != null && i < position; i++) {
-            prev = temp;
-            temp = temp.next;
+        // Store the head reference in a temporary variable
+        Node last = head;
+
+        // Traverse till the last node
+        while (last.next != null) {
+            last = last.next;
         }
 
-        // If given position is found, delete node
-        if (temp != null) {
-            prev.next = temp.next;
-        }
-        else {
-            System.out.println("Data not present");
-        }
+        // Change the next pointer of the 
+        // last node to point to the new node
+        last.next = newNode;
 
-         return head;
+        // Return the head of the list
+        return head;
     }
 
-    // Function to print the linked list
-    public static void printList(Node head)
-    {
-        while (head != null) {
-            System.out.print(head.data + " -> ");
-            head = head.next;
+    // This function prints the contents of 
+    // the linked list starting from the head
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(" " + node.data);
+            node = node.next;
         }
-        System.out.println("null");
     }
 
     // Driver code
-    public static void main(String[] args)
-    {
-        // Creating a static linked list
-        // 1 -> 2 -> 3 -> 4 -> 5 -> null
-        Node head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(4);
-        head.next.next.next.next = new Node(5);
+    public static void main(String[] args) {
+      
+        // Create a hard-coded linked list: 
+        // 2 -> 3 -> 4 -> 5 -> 6
+        Node head = new Node(2);
+        head.next = new Node(3);
+        head.next.next = new Node(4);
+        head.next.next.next = new Node(5);
+        head.next.next.next.next = new Node(6);
 
-        // Print original list
-        System.out.print("Original list: ");
+        System.out.print("Created Linked list is:");
         printList(head);
 
-        // Deleting node at position 2
-        int position = 2;
-        head = deleteNode(head, position);
+        // Example of appending a node at the end
+        head = append(head, 1);
 
-        // Print list after deletion
-        System.out.print("List after deletion: ");
+        System.out.print("\nAfter inserting 1 at the end:");
         printList(head);
     }
 }
