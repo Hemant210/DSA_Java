40a1ff62a6050851585aa225328630e68949ab09
Hemant210 <hemantpatil.2103@gmail.com>
2025-07-20 10:03:40 +0530

    Add stack practice file

--- Stack.java
+++ Stack.java
@@ -1,79 +1,45 @@
 import java.util.ArrayList;
 
 public class Stack {
-    // Linked List Implementation of Stack (Commented)
-    // Node class represents a single element in the linked list
-    static class Node {
-        int data;       // data part of the node
-        Node next;      // reference to the next node
+    // // Linked List Implementation of Stack
+    // // Node class represents a single element in the linked list
+    // static class Node {
+    //     int data;       // data part of the node
+    //     Node next;      // reference to the next node
 
-        public Node(int data) {
-            this.data = data;
-            next = null;
-        }
-    }
+    //     public Node(int data) {
+    //         this.data = data;
+    //         next = null;
+    //     }
+    // }
 
-    // Stack implementation using linked list
-    static class Stack_class {
-        public static Node head; // top of the stack
-
-        // Stack is empty if head is null
-        public static boolean isEmpty() {
-            return head == null;
-        }
-
-        // Push: add element to top
-        public static void push(int data) {
-            Node newNode = new Node(data);
-            if (isEmpty()) {
-                head = newNode;
-                return;
-            }
-            newNode.next = head; // new node points to current head
-            head = newNode;      // head now becomes the new node
-        }
-
-        // Pop: remove and return top element
-        public static int pop() {
-            if (isEmpty()) {
-                return -1; // return -1 if stack is empty
-            }
-            int top = head.data;
-            head = head.next; // move head to next node
-            return top;
-        }
-
-        // Peek: return top element without removing
-        public static int peek() {
-            if (isEmpty()) {
-                return -1;
-            }
-            return head.data;
-        }
-    }
-    // ArrayList Implementation of Stack (Active Version)
-
+    // // Stack implementation using linked list
     // static class Stack_class {
-    //     // Using ArrayList to store elements
-    //     static ArrayList<Integer> list = new ArrayList<>();
+    //     public static Node head; // top of the stack
 
+    //     // Stack is empty if head is null
     //     public static boolean isEmpty() {
-    //         return list.size() == 0;
+    //         return head == null;
     //     }
 
-    //     // Push: add element to end of list (top of stack)
+    //     // Push: add element to top
     //     public static void push(int data) {
-    //         list.add(data);
+    //         Node newNode = new Node(data);
+    //         if (isEmpty()) {
+    //             head = newNode;
+    //             return;
+    //         }
+    //         newNode.next = head; // new node points to current head
+    //         head = newNode;      // head now becomes the new node
     //     }
 
     //     // Pop: remove and return top element
     //     public static int pop() {
     //         if (isEmpty()) {
-    //             return -1; // if stack is empty
+    //             return -1; // return -1 if stack is empty
     //         }
-    //         // get and remove last element
-    //         int top = list.get(list.size() - 1);
-    //         list.remove(list.size() - 1);
+    //         int top = head.data;
+    //         head = head.next; // move head to next node
     //         return top;
     //     }
 
@@ -82,9 +48,43 @@ public class Stack {
     //         if (isEmpty()) {
     //             return -1;
     //         }
-    //         return list.get(list.size() - 1);
+    //         return head.data;
     //     }
     // }
+    
+    // ArrayList Implementation of Stack
+    static class Stack_class {
+        // Using ArrayList to store elements
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isEmpty() {
+            return list.size() == 0;
+        }
+
+        // Push: add element to end of list (top of stack)
+        public static void push(int data) {
+            list.add(data);
+        }
+
+        // Pop: remove and return top element
+        public static int pop() {
+            if (isEmpty()) {
+                return -1; // if stack is empty
+            }
+            // get and remove last element
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+
+        // Peek: return top element without removing
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return list.get(list.size() - 1);
+        }
+    }
 
     public static void main(String[] args) {
         Stack_class s = new Stack_class(); // create stack object
