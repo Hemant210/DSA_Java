5d0cbc3b0a4089c0ae142a1590574302de0923b2
Hemant210 <hemantpatil.2103@gmail.com>
2025-07-20 11:05:00 +0530

    Update all files 20.07.2025

--- Stack.java
+++ Stack.java
@@ -4,54 +4,54 @@ public class Stack {
     // // Linked List Implementation of Stack
     // // Node class represents a single element in the linked list
     // static class Node {
-    //     int data;       // data part of the node
-    //     Node next;      // reference to the next node
+    // int data; // data part of the node
+    // Node next; // reference to the next node
 
-    //     public Node(int data) {
-    //         this.data = data;
-    //         next = null;
-    //     }
+    // public Node(int data) {
+    // this.data = data;
+    // next = null;
+    // }
     // }
 
     // // Stack implementation using linked list
     // static class Stack_class {
-    //     public static Node head; // top of the stack
+    // public static Node head; // top of the stack
 
-    //     // Stack is empty if head is null
-    //     public static boolean isEmpty() {
-    //         return head == null;
-    //     }
-
-    //     // Push: add element to top
-    //     public static void push(int data) {
-    //         Node newNode = new Node(data);
-    //         if (isEmpty()) {
-    //             head = newNode;
-    //             return;
-    //         }
-    //         newNode.next = head; // new node points to current head
-    //         head = newNode;      // head now becomes the new node
-    //     }
-
-    //     // Pop: remove and return top element
-    //     public static int pop() {
-    //         if (isEmpty()) {
-    //             return -1; // return -1 if stack is empty
-    //         }
-    //         int top = head.data;
-    //         head = head.next; // move head to next node
-    //         return top;
-    //     }
-
-    //     // Peek: return top element without removing
-    //     public static int peek() {
-    //         if (isEmpty()) {
-    //             return -1;
-    //         }
-    //         return head.data;
-    //     }
+    // // Stack is empty if head is null
+    // public static boolean isEmpty() {
+    // return head == null;
     // }
-    
+
+    // // Push: add element to top
+    // public static void push(int data) {
+    // Node newNode = new Node(data);
+    // if (isEmpty()) {
+    // head = newNode;
+    // return;
+    // }
+    // newNode.next = head; // new node points to current head
+    // head = newNode; // head now becomes the new node
+    // }
+
+    // // Pop: remove and return top element
+    // public static int pop() {
+    // if (isEmpty()) {
+    // return -1; // return -1 if stack is empty
+    // }
+    // int top = head.data;
+    // head = head.next; // move head to next node
+    // return top;
+    // }
+
+    // // Peek: return top element without removing
+    // public static int peek() {
+    // if (isEmpty()) {
+    // return -1;
+    // }
+    // return head.data;
+    // }
+    // }
+
     // ArrayList Implementation of Stack
     static class Stack_class {
         // Using ArrayList to store elements
@@ -98,7 +98,7 @@ public class Stack {
         // Print and pop each element until stack is empty
         while (!s.isEmpty()) {
             System.out.println(s.peek()); // show top element
-            s.pop();                      // remove top element
+            s.pop(); // remove top element
         }
     }
 }
