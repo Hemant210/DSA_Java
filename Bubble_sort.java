24a981d3b1a011c892d0754f2af3943c75e0e986
Hemant210 <hemantpatil.2103@gmail.com>
2025-07-21 11:29:01 +0530

    Update all files

--- Bubble_sort.java
+++ Bubble_sort.java
@@ -35,7 +35,7 @@ public class Bubble_sort {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
-    }
+    }   
 
     public static void main(String[] args) {
         int arr[] = { 7, 8, 3, 2, 1 };
