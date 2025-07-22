e762210ba83655240b30431b1dd20d713174cda0
Hemant210 <hemantpatil.2103@gmail.com>
2025-07-22 11:15:08 +0530

    Queue using 2 stacks - code

--- Practice/Queue_practice.java
+++ Practice/Queue_practice.java
@@ -2,11 +2,71 @@ package Practice;
 
 import java.util.*;
 
+//Java Collection Framework
+// public class Queue_practice {
+
+//     public static void main(String[] args) {
+//         Queue<Integer> q = new ArrayDeque<>();
+
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         q.add(4);
+//         q.add(5);
+
+//         System.out.println("Queue elements:");
+//         // Print and remove all elements one by one
+//         while (!q.isEmpty()) {
+//             System.out.println("Peek: " + q.peek());
+//             q.remove();
+
+//         }
+//     }
+// }
+
+//Queue using 2 stacks
 public class Queue_practice {
+    static class queue {
+        static Stack<Integer> s1 = new Stack<>();
+        static Stack<Integer> s2 = new Stack<>();
+
+        public static boolean isEmpty() {
+            return s1.isEmpty();
+        }
+
+        public static void add(int data) {
+            while(!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+
+            s1.push(data);
+
+            while(!s2.isEmpty()) {
+                s1.push(s2.pop());
+            }
+        }
+
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("Empyt");
+                return -1;
+            }
+
+            return s1.pop();
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("Empyt");
+                return -1;
+            }
+
+            return s1.peek();
+        }
+    }
 
     public static void main(String[] args) {
-        Queue<Integer> q = new ArrayDeque<>();
-
+        queue q = new queue();
         q.add(1);
         q.add(2);
         q.add(3);
@@ -18,7 +78,6 @@ public class Queue_practice {
         while (!q.isEmpty()) {
             System.out.println("Peek: " + q.peek());
             q.remove();
-
         }
     }
 }
