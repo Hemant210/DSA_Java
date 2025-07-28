eb42f1bad21d9690b2a077f444cac7c09840b977
Hemant210 <hemantpatil.2103@gmail.com>
2025-07-28 10:53:31 +0530

    Build a Tree from its Preorder traversal - code

---
+++ Binary_Tree.java
@@ -0,0 +1,36 @@
+//Build a Tree from its Preorder traversal
+public class Binary_Tree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class Binarytrees{
+        static int idx = -1;
+        public static Node buildtree(int nodes[]){
+        idx++;
+        if (nodes[idx] == -1) {
+            return null;
+        }
+
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildtree(nodes);
+        newNode.right = buildtree(nodes);
+
+        return newNode;
+        }
+    }
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        Binarytrees tree = new Binarytrees();
+        Node root = tree.buildtree(nodes);
+        System.out.println(root.data);
+    }
+}
