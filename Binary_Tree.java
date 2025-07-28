88af184bd5b6c71a268f2e1175de89c94f30fce4
Hemant210 <hemantpatil.2103@gmail.com>
2025-07-28 10:58:00 +0530

    Tree Traversals Preorder - code added

--- Binary_Tree.java
+++ Binary_Tree.java
@@ -27,10 +27,20 @@ public class Binary_Tree {
         return newNode;
         }
     }
+
+    public static void preorder(Node root){
+        if (root == null) {
+            return;
+        }
+        System.out.println(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         Binarytrees tree = new Binarytrees();
         Node root = tree.buildtree(nodes);
-        System.out.println(root.data);
+        //System.out.println(root.data);
+        preorder(root);
     }
 }
