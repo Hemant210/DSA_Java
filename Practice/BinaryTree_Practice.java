d88f1178d9d2a965d8d076b9321a568e59f13a1a
Hemant210 <hemantpatil.2103@gmail.com>
2025-07-29 11:04:40 +0530

    Count of Nodes code - solution

---
+++ Practice/BinaryTree_Practice.java
@@ -0,0 +1,51 @@
+package Practice;
+
+//Count of Nodes
+public class BinaryTree_Practice {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class Binarytrees {
+        static int idx = -1;
+
+        public static Node buildtree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildtree(nodes);
+            newNode.right = buildtree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static int countofNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftNode = countofNodes(root.left);
+        int rightNode = countofNodes(root.right);
+
+        return leftNode + rightNode + 1;
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Binarytrees tree = new Binarytrees();
+        Node root = tree.buildtree(nodes);
+        System.out.println("Number of Nodes :- " + countofNodes(root));
+    }
+}
