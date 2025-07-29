2b81741f0d564fe2f0e1457a15fa1b2aa2216d8f
Hemant210 <hemantpatil.2103@gmail.com>
2025-07-29 10:54:01 +0530

    Add Tree Traversals Level order - Code

--- Binary_Tree.java
+++ Binary_Tree.java
@@ -1,4 +1,8 @@
 //Build a Tree from its Preorder traversal
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Binary_Tree {
     static class Node {
         int data;
@@ -61,6 +65,35 @@ public class Binary_Tree {
         System.out.println(root.data + " ");
     }
 
+    // Tree Traversals Level order - DFS
+    public static void Levelorder(Node root){
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+            if (currNode == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currNode.data + " ");
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         Binarytrees tree = new Binarytrees();
@@ -68,6 +101,7 @@ public class Binary_Tree {
         // System.out.println(root.data);
         // preorder(root);
         //inorder(root);
-        Postorder(root);
+        //Postorder(root);
+        Levelorder(root);
     }
 }
