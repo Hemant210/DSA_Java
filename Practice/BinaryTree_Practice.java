2c75fbf3eab59993a7a47b724a701a4e6ef54baf
Hemant210 <hemantpatil.2103@gmail.com>
2025-07-31 10:43:51 +0530

    Calculate height of Tree - code

--- Practice/BinaryTree_Practice.java
+++ Practice/BinaryTree_Practice.java
@@ -65,12 +65,26 @@ public class BinaryTree_Practice {
 
         return Math.max(leftheight, rightheight + 1);
     }
+
+    //Calculate diameter of Tree
+    public static int diameter(Node root){
+        if (root == null) {
+            return 0;
+        }
+
+        int dia1 = diameter(root.left);
+        int dia2 = diameter(root.right);
+        int dia3 = height(root.left) + height(root.right) + 1;
+
+        return Math.max(dia3, Math.max(dia1, dia2));
+    }
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         Binarytrees tree = new Binarytrees();
-        Node root = tree.buildtree(nodes);
+        Node root = tree.buildtree(nodes);  
         System.out.println("Number of Nodes :- " + countofNodes(root));
         System.out.println("Sum of Nodes :- " + sumofNodes(root));
         System.out.println("Height of Tree :- " + height(root));
+        System.out.println("Diameter of Tree :- " + diameter(root));
     }
 }
