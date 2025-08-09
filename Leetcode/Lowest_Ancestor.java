28bc4afba944179b3e857a8e44af81d61e392df8
Hemant210 <hemantpatil.2103@gmail.com>
2025-08-09 11:05:19 +0530

    Lowest Common Ancestor of a Binary Tree - code solution

--- Leetcode/Lowest_Ancestor.java
+++ Leetcode/Lowest_Ancestor.java
@@ -13,61 +13,60 @@ public class Lowest_Ancestor {
     }
 
     // public TreeNode lowestCommonAncestor(TreeNode root, int p, int q) {
-    // if (root == null) {
-    // return null;
+    //     if (root == null) {
+    //         return null;
+    //     }
+
+    //     // If root matches either p or q, return root
+    //     if (root.val == p || root.val == q) {
+    //         return root;
+    //     }
+
+    //     TreeNode left = lowestCommonAncestor(root.left, p, q);
+    //     TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+    //     if (left == null) {
+    //         return right;
+    //     } else if (right == null) {
+    //         return left;
+    //     }
+
+    //     return root;
     // }
-
-    // // If root matches either p or q, return root
-    // if (root.val == p || root.val == q) {
-    // return root;
-    // }
-
-    // TreeNode left = lowestCommonAncestor(root.left, p, q);
-    // TreeNode right = lowestCommonAncestor(root.right, p, q);
-
-    // if (left == null) {
-    // return right;
-    // } else if (right == null) {
-    // return left;
-    // }
-
-    // return root;
-    // }
-    
-    public TreeNode lowestCommonAncestor(TreeNode root, int p, int q) {
-        // Ensure p is less than or equal to q
-        if (p > q) {
-            int temp = p;
-            p = q;
-            q = temp;
-        }
-
-        while (root != null) {
-            if (root.val > q) {
-                root = root.left;
-            } else if (root.val < p) {
-                root = root.right;
-            } else {
-                return root; // This is the LCA
-            }
-        }
-
-        return null;
+public TreeNode lowestCommonAncestor(TreeNode root, int p, int q){
+    // Ensure p is less than or equal to q
+    if (p > q) {
+        int temp = p;
+        p = q;
+        q = temp;
     }
+
+    while (root != null) {
+        if (root.val > q) {
+            root = root.left;
+        } else if (root.val < p) {
+            root = root.right;
+        } else {
+            return root; // This is the LCA
+        }
+    }
+
+    return null;
+}
 
     public static void main(String[] args) {
         Lowest_Ancestor solution = new Lowest_Ancestor();
 
         // Constructing a sample binary tree:
         /*
-         * 3
-         * / \
-         * 5 1
-         * / \ / \
-         * 6 2 7 4
-         * / \
-         * 0 8
-         */
+               3
+             /   \
+           5       1
+         /   \    / \
+        6     2  7   4
+             / \
+            0   8
+        */
         TreeNode root = new TreeNode(3);
         root.left = new TreeNode(5);
         root.right = new TreeNode(1);
