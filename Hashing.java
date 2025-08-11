c2b29555480ad269eaa982db8fbca45efc36a613
Hemant210 <hemantpatil.2103@gmail.com>
2025-08-11 11:22:19 +0530

    Add Delete,size & Interator Function

--- Hashing.java
+++ Hashing.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class Hashing {
 
@@ -10,7 +11,8 @@ public class Hashing {
         set.add(1);
         set.add(2);
         set.add(3);
-        set.add(1);
+        set.add(1);//Not Insert Duplicate value
+        set.add(4);
         
         //Search-contains element
         if (set.contains(1)) {
@@ -19,5 +21,25 @@ public class Hashing {
         if (!set.contains(6)) {
             System.out.println("Set does Not contains");
         }
+
+        //Delete element 
+        //set.remove(1);
+        if (!set.contains(1)) {
+            System.out.println("Set does Not contains we deleted 1");
+        }
+
+        //Print set size
+        System.out.println("Size of set is :- " + set.size());
+
+        //Print all Elemment
+        System.out.println(set);
+
+        //Iterator in Hashset
+        Iterator IT = set.iterator();
+        //HasNext,Next
+        while (IT.hasNext()) {
+            System.out.println(IT.next());
+        }
+        
     }
 }
