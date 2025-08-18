defff2d9f349179b41f5b9eed03dc8227f4a9a89
Hemant210 <hemantpatil.2103@gmail.com>
2025-08-18 09:23:22 +0530

    Majority Element - Hashmap (n/2)

--- Practice/Hashing_practice.java
+++ Practice/Hashing_practice.java
@@ -2,6 +2,7 @@ package Practice;
 
 import java.util.*;
 
+
 // public class Hashing_practice {
 
 //     // Custom HashMap implementation
@@ -223,29 +224,53 @@ import java.util.*;
 //     }
 // }
 
+//Majority Element II - Hashmap 
+//Time complexity :- O(n)
+// public class Hashing_practice {
+//     public static void majorityelement(int nums[]){
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int n = nums.length;
+
+//         for(int i=0; i<n; i++){
+//             if (map.containsKey(nums[i])) {
+//                 map.put(nums[i], map.get(nums[i]) + 1);
+//             } else {
+//                 map.put(nums[i], 1);
+//             }
+//         }
+
+//         for(int key : map.keySet()){
+//             if(map.get(key) > n/3) {
+//                 System.out.println(key);
+//             }
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int nums[] = {1,3,2,5,1,3,1,5,1};
+//         majorityelement(nums);
+//     }
+// }
+
 //Majority Element - Hashmap 
 //Time complexity :- O(n)
 public class Hashing_practice {
-    public static void majorityelement(int nums[]){
+    public static void majorityelement(int nums[]) {
         HashMap<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
 
-        for(int i=0; i<n; i++){
-            if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1);
-            } else {
-                map.put(nums[i], 1);
-            }
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        for(int key : map.keySet()){
-            if(map.get(key) > n/3) {
+        for (int key : map.keySet()) {
+            if (map.get(key) > n / 2) {
                 System.out.println(key);
             }
         }
     }
+
     public static void main(String[] args) {
-        int nums[] = {1,3,2,5,1,3,1,5,1};
+        int nums[] = {2,2,1,1,1,2,2};
         majorityelement(nums);
     }
 }
