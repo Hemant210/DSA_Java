b0e12d0f29bfe0d07c077244cef15c74023b61e9
Hemant210 <hemantpatil.2103@gmail.com>
2025-08-19 16:04:02 +0530

    Find Itinerary from tickets - code

--- Practice/Hashing_practice.java
+++ Practice/Hashing_practice.java
@@ -351,28 +351,64 @@ import java.util.*;
 // }
 
 //Instersection of 2 arrays print array
-public class Hashing_practice {
-    public static void instersection(int arr1[], int arr2[]){
-        HashSet <Integer> set = new HashSet<>();
-        HashSet <Integer> intersectionSet = new HashSet<>();
+// public class Hashing_practice {
+//     public static void instersection(int arr1[], int arr2[]){
+//         HashSet <Integer> set = new HashSet<>();
+//         HashSet <Integer> intersectionSet = new HashSet<>();
 
-        for(int i=0; i < arr1.length; i++){
-            set.add(arr1[i]);
+//         for(int i=0; i < arr1.length; i++){
+//             set.add(arr1[i]);
+//         }
+
+//         for(int j=0; j < arr2.length; j++){
+//             if (set.contains(arr2[j])) {
+//                 intersectionSet.add(arr2[j]);
+//                 set.remove(arr2[j]);
+//             }
+//         }
+
+//         System.out.println(intersectionSet);
+//     }
+//     public static void main(String[] args) {
+//         int arr1[] = {7,3,9};
+//         int arr2[] = {6,3,9,2,9,4};
+
+//         instersection(arr1, arr2);
+//     }
+// }
+
+// Find Itinerary from tickets
+public class Hashing_practice {
+    public static String getStart(HashMap<String, String> tik){
+        HashMap<String, String> revMap = new HashMap<>();
+
+        for(String key : tik.keySet()){
+            //key -> value
+            revMap.put(tik.get(key), key);
         }
 
-        for(int j=0; j < arr2.length; j++){
-            if (set.contains(arr2[j])) {
-                intersectionSet.add(arr2[j]);
-                set.remove(arr2[j]);
+        for(String key : tik.keySet()){
+            if (!revMap.containsKey(key)) {
+                return key;
             }
         }
 
-        System.out.println(intersectionSet);
+        return null;
     }
-    public static void main(String[] args) {
-        int arr1[] = {7,3,9};
-        int arr2[] = {6,3,9,2,9,4};
+    public static void main(String[] args) {       
+        HashMap<String, String> tickets = new HashMap<>();
+        tickets.put("Chennai", "Bengaluru");
+        tickets.put("Mumbai", "Delhi");
+        tickets.put("Goa", "Chennai");
+        tickets.put("Delhi", "Goa");
 
-        instersection(arr1, arr2);
+        String start = getStart(tickets);
+
+        while (tickets.containsKey(start)) {
+            System.out.println(start + "->");
+            start = tickets.get(start);
+        }
+
+        System.out.println(start);
     }
 }
