2be91268210030c4810e9ea7cc8e181b4071930f
Hemant210 <hemantpatil.2103@gmail.com>
2025-08-31 23:59:53 +0530

    Update trie.java code

--- Trie.java
+++ Trie.java
@@ -1,1 +1,21 @@
-dd
+public class Trie {
+    static class Node {
+        Node[] children;
+        boolean eow;
+
+        public Node(){
+            children = new Node[26];
+            for(int i=0; i < 26; i++){
+                children[i] = null;
+            }
+
+            eow = false;
+        }
+    }
+
+    static Node root = new Node();
+    public static void main(String[] args) {
+        String wordsp[] = {"the", "a", "there", "their", "any"};
+    }
+}
+ 
