2845552dd0301fbffe22ebe0428169a9c9e0019f
Hemant210 <hemantpatil.2103@gmail.com>
2025-09-03 19:53:18 +0530

    Insert a word into Trie - code

--- Trie.java
+++ Trie.java
@@ -35,7 +35,11 @@ public class Trie {
         }
     }
 
-
+    //Search a word into Trie
+    public static String search(String word){
+        
+    }
+    
     public static void main(String[] args) {
         String words[] = { "the", "a", "there", "their", "any" };
 
