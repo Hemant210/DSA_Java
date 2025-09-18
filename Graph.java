cee35c17e1ad43989898fa249c6877015afe3c83
Hemant210 <hemantpatil.2103@gmail.com>
2025-09-18 09:31:53 +0530

    all destination vertices connected from src = 2 with weight

--- Graph.java
+++ Graph.java
@@ -6,10 +6,12 @@ public class Graph {
     static class Edge {
         int src;   // source vertex
         int dest;  // destination vertex
+        int wght;  // Weight Vertex
 
-        public Edge(int s, int d){
+        public Edge(int s, int d, int w){
             this.src = s;
             this.dest = d;
+            this.wght = w;
         }
     }
 
@@ -23,20 +25,20 @@ public class Graph {
 
         // Step 2: Add edges (directed graph)
         // vertex 0 -> 2
-        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 2,2));
         
         // vertex 1 -> 2 and 1 -> 3
-        graph[1].add(new Edge(1, 2));
-        graph[1].add(new Edge(1, 3));
+        graph[1].add(new Edge(1, 2,10));
+        graph[1].add(new Edge(1, 3,0));
 
         // vertex 2 -> 0, 2 -> 1, 2 -> 3
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 1));
-        graph[2].add(new Edge(2, 3));
+        graph[2].add(new Edge(2, 0,2));
+        graph[2].add(new Edge(2, 1,10));
+        graph[2].add(new Edge(2, 3,-1));
 
         // vertex 3 -> 1, 3 -> 2
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 2));
+        graph[3].add(new Edge(3, 1,0));
+        graph[3].add(new Edge(3, 2,-1));
     }
 
     public static void main(String[] args) {
@@ -52,7 +54,7 @@ public class Graph {
         // neighbours = all destination vertices connected from src = 2
         for(int i = 0; i < graph[2].size(); i++){
             Edge e = graph[2].get(i);   // get edge from adjacency list
-            System.out.println(e.dest + " "); // print destination node
+            System.out.println(e.dest + "," + e.wght); // print destination node
         }
     }
 }
