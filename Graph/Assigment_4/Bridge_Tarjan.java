295537ad6f6ddc9d8d75c0d6d75e8ebf877896e4
Hemant210 <hemantpatil.2103@gmail.com>
2025-11-07 17:58:11 +0530

    Bridge in Graph (Tarjan’s Algorithm)

---
+++ Graph/Assigment_4/Bridge_Tarjan.java
@@ -0,0 +1,98 @@
+package Graph.Assigment_4;
+
+import java.util.ArrayList;
+
+//Bridge in Graph (Tarjan’s Algorithm)
+public class Bridge_Tarjan {
+
+    // Inner class to represent an edge between two nodes
+    static class Edge {
+        int src, dest;
+        Edge(int s, int d) {
+            src = s;
+            dest = d;
+        }
+    }
+
+    // Function to create a sample graph
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // Example Graph (Undirected)
+        // 0---1---2
+        // |   |
+        // 3---4
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 3));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 4));
+
+        graph[2].add(new Edge(2, 1));
+
+        graph[3].add(new Edge(3, 0));
+        graph[3].add(new Edge(3, 4));
+
+        graph[4].add(new Edge(4, 1));
+        graph[4].add(new Edge(4, 3));
+    }
+
+    // DFS function for Tarjan's Algorithm to find bridges
+    public static void dfs(ArrayList<Edge>[] graph, int curr, int par,
+                           boolean[] vis, int[] dt, int[] low, int time) {
+
+        vis[curr] = true;
+        dt[curr] = low[curr] = ++time;  // discovery & low time
+
+        for (Edge e : graph[curr]) {
+            int neigh = e.dest;
+
+            // Skip the edge to parent
+            if (neigh == par) continue;
+
+            // Case 1: Back Edge found
+            if (vis[neigh]) {
+                low[curr] = Math.min(low[curr], dt[neigh]);
+            }
+            // Case 2: Forward Edge (DFS Tree Edge)
+            else {
+                dfs(graph, neigh, curr, vis, dt, low, time);
+
+                // After returning, update low value
+                low[curr] = Math.min(low[curr], low[neigh]);
+
+                // Bridge condition
+                if (dt[curr] < low[neigh]) {
+                    System.out.println("Bridge : " + curr + " --- " + neigh);
+                }
+            }
+        }
+    }
+
+    // Function to find all bridges in the graph
+    public static void getBridge(ArrayList<Edge>[] graph, int V) {
+        int[] dt = new int[V];     // discovery time
+        int[] low = new int[V];    // lowest discovery time reachable
+        boolean[] vis = new boolean[V];
+        int time = 0;
+
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs(graph, i, -1, vis, dt, low, time);
+            }
+        }
+    }
+
+    // Driver Function
+    public static void main(String[] args) {
+        int V = 5; // number of vertices
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+
+        System.out.println("Bridges in the Graph:");
+        getBridge(graph, V);
+    }
+}
