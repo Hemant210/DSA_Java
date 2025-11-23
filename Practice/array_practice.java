802aa64ebfa16392ad13d6399330baef2e65ec92
Hemant210 <hemantpatil.2103@gmail.com>
2025-11-23 10:40:39 +0530

    Update array practice

--- Practice/array_practice.java
+++ Practice/array_practice.java
@@ -42,7 +42,7 @@ public class array_practice {
         int ans = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            if (freq == 0) { // Use == for comparison
+            if (freq == 0) {
                 ans = nums[i];
             }
 
