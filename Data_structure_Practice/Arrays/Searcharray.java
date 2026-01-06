3869d4d70fb1ed0457e3923de159e2ca94f32c0a
Hemant210 <hemantpatil.2103@gmail.com>
2026-01-06 11:09:48 +0530

    Search in Rotated Sorted Array

---
+++ Data_structure_Practice/Arrays/Searcharray.java
@@ -0,0 +1,18 @@
+package Data_structure_Practice.Arrays;
+
+public class Searcharray {
+    public static int Find_rotedarray(int[] arr, int target){
+        for(int i = 0; i < arr.length; i++){
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        System.out.println(Find_rotedarray(arr, 9));
+    }
+}
