8597c75327876c43c2e420cfee2c29aba274889d
Hemant210 <hemantpatil.2103@gmail.com>
2026-01-09 22:08:45 +0530

    Find Minimum with Time Complexity: O(n) & O(1) Space

--- Data_structure_Practice/Arrays/Findminimum.java
+++ Data_structure_Practice/Arrays/Findminimum.java
@@ -1,8 +1,9 @@
 package Data_structure_Practice.Arrays;
 
 public class Findminimum {
-    public static int findmin(int[] nums){
-        for(int i = 0; i < nums.length; i++){
+    // Time Complexity: O(n) & O(1) Space
+    public static int findmin(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[0] > nums[i]) {
                 nums[0] = nums[i];
             }
@@ -10,8 +11,20 @@ public class Findminimum {
 
         return nums[0];
     }
+
+    public static int findmini(int[] nums) {
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            ans = Math.min(ans, val);
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,0,1,2};
-        System.out.println(findmin(arr));
+        int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
+        System.out.println(findmini(arr));
     }
 }
