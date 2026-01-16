b1c602206b3ffdc265ee76a9da289fe27f5c3382
Hemant210 <hemantpatil.2103@gmail.com>
2026-01-16 23:00:08 +0530

    Valid Palindrome - brute force

---
+++ Data_structure_Practice/Strings/Validpalindrome.java
@@ -0,0 +1,24 @@
+package Data_structure_Practice.Strings;
+
+public class Validpalindrome {
+    public static boolean isPalindrome(String s){
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(right) != s.charAt(left)) {
+                return false;
+            } 
+
+            left++;
+            right--;
+        }
+
+        return true ;
+    }
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+}
