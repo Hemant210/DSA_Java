88da2218ad39e4d445f1127c96bd13a86b39abab
Hemant210 <hemantpatil.2103@gmail.com>
2026-01-23 23:07:09 +0530

    Valid parentheses using stack

---
+++ Data_structure_Practice/Strings/Check_Parentheses.java
@@ -0,0 +1,34 @@
+package Data_structure_Practice.Strings;
+
+import java.util.Stack;
+
+public class Check_Parentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') { //opening
+                st.push(s.charAt(i));
+            } else { //closing
+                if (st.size() == 0) {
+                    return false;
+                }
+
+                if ((st.peek() == '{' && s.charAt(i) == '}') ||
+                    (st.peek() == '[' && s.charAt(i) == ']') ||
+                    (st.peek() == '(' && s.charAt(i) == ')')) {
+                        st.pop();
+                } else { //no match
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+    }
+}
