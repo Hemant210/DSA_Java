834247107da825c7cfec12ddad618da6523fbc4d
Hemant210 <hemantpatil.2103@gmail.com>
2026-01-29 23:47:13 +0530

    Brute Force Approch - Print duplicates

--- Data_structure_Practice/Strings/printDuplicates.java
+++ Data_structure_Practice/Strings/printDuplicates.java
@@ -4,31 +4,27 @@ import java.util.Arrays;
 
 //Print all the duplicate characters in a string
 public class printDuplicates {
-    // Time complexity :- O(n*log(n)) and Space :- O(1)
+    //Time complexity :- O(n*log(n)) and Space :- O(1)
     public static void printduplicate(String s) {
         char[] arr = s.toCharArray();
-
         Arrays.sort(arr);
 
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < arr.length; i++) {
             int count = 1;
 
-            while (i + count < arr.length && arr[i] == arr[i + count]) {
+            while (i + 1 < arr.length && arr[i] == arr[i + 1]) {
                 count++;
+                i++;
             }
 
             if (count > 1) {
                 System.out.print("['" + arr[i] + "', " + count + "], ");
             }
-
-            i += count;
         }
-
     }
 
     public static void main(String[] args) {
         String s = "geeksforgeeks";
-
         printduplicate(s);
     }
 }
