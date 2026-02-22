4f1e931d9313dfa40aba9396921f3883a35d6c88
Hemant210 <hemantpatil.2103@gmail.com>
2026-02-22 22:22:38 +0530

    New Approch Binary search & Recursion approch

---
+++ Data_structure_Practice/Arrays/Painter_partion.java
@@ -0,0 +1,7 @@
+package Data_structure_Practice.Arrays;
+
+public class Painter_partion {
+    public static void main(String[] args) {
+        
+    }
+}
--- Data_structure_Practice/Arrays/Searcharray.java
+++ Data_structure_Practice/Arrays/Searcharray.java
@@ -2,7 +2,8 @@ package Data_structure_Practice.Arrays;
 
 public class Searcharray {
 
-    // Time Complexity: O(n) & O(1) Space
+    //Linear Search Approch
+    // Time Complexity: O(n) & Space Complexity : O(1)
     public static int Find_rotedarray(int[] arr, int target) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
@@ -13,9 +14,58 @@ public class Searcharray {
         return -1;
     }
 
+
+    //Binary Search Approch
+    //Time Complexity :- O(log n) & Space Complexity : O(1)
+    public static int Find_Element(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        
+        while (start <= end) {
+            //int mid = (start + end)/2; //Beginer
+            int mid = start + (end - start)/2; //Advance solve overflow problem
+
+            if (target > arr[mid]) {
+                start = mid + 1; // First Phase of Array
+                
+            }
+            else if(target < arr[mid]){
+                end = mid - 1; // Second phase of Array
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+    //Recursion Approch
+    //Time Complexity  : O(log n) & Space Complexity : O(1)
+    public static int Recursion_binarysearch(int[] arr, int tar, int st, int en){
+        if (st <= en) {
+            int mid = st + (en - st)/2;
+
+            if (tar > arr[mid]) {
+                return Recursion_binarysearch(arr, tar, mid + 1, en);
+            }
+
+            else if (tar < arr[mid]) {
+                return Recursion_binarysearch(arr, tar, st, mid - 1);
+            }
+
+            return mid;
+        }
+
+        return -1;
+
+    }
     public static void main(String[] args) {
-        int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
-        System.out.println(Find_rotedarray(arr, 9));
+        int[] arr = { -1, 0, 3, 5, 9, 12 };
+        int target = 0;
+        //System.out.println(Find_rotedarray(arr, 9));
+        //System.out.println(Find_Element(arr, 12));
+        System.out.println(Recursion_binarysearch(arr, target, 0, arr.length - 1));
     }
 }
  
