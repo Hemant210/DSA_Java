5268ed0c8226841b8ea354228e5bc84eb555f78a
Hemant210 <hemantpatil.2103@gmail.com>
2026-02-23 19:37:19 +0530

    Neive Approch with Time complexity :- O(n)

--- Data_structure_Practice/Binary_Search/Peak_IndexMountain.java
+++ Data_structure_Practice/Binary_Search/Peak_IndexMountain.java
@@ -1,6 +1,25 @@
 package Data_structure_Practice.Binary_Search;
 
 public class Peak_IndexMountain {
+
+    //Naive Approch
+    //Time Complexity :- O(n) & & Space complexity: O(1)
+    public static int peakArray(int[] arr){
+        int n = arr.length;
+        int max = arr[0];
+        int index = 0;
+
+        for(int i = 0; i < n; i++){
+            if (arr[i] > max) {
+                max = arr[i];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+
+    //Binary Search Approch
     //Time Complexity :- O(log(n)) & & Space complexity: O(1)
     public static int peakIndexInMountainArray(int[] arr){
         int low = 0;
@@ -21,5 +40,6 @@ public class Peak_IndexMountain {
     public static void main(String[] args) {
        int[] arr = {0,1,0};
        System.out.println(peakIndexInMountainArray(arr));
+       //System.out.println(peakArray(arr));
     }
 }
