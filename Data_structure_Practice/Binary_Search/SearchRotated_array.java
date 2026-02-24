b7b17adff9c64dc0fecf745bf4d887b63f48c218
Hemant210 <hemantpatil.2103@gmail.com>
2026-02-24 23:17:19 +0530

    Search in Rotated Sorted Array

---
+++ Data_structure_Practice/Binary_Search/SearchRotated_array.java
@@ -0,0 +1,44 @@
+package Data_structure_Practice.Binary_Search;
+
+public class SearchRotated_array {
+    // Time Complexity :- O(log n) & Space Complexity : O(1)
+    public static int Find_Index(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            // Left half is sorted
+            if (arr[start] <= arr[mid]) {
+                if (arr[start] <= target && target <= arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+
+            // Right half is sorted
+            else {
+                if (arr[mid] <= target && target <= arr[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 0;
+
+        System.out.println(Find_Index(arr, target));
+    }
+}
