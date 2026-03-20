8111129695dac6a5a0376b8fb8bbedc2bc9c71b6
Hemant210 <hemantpatil.2103@gmail.com>
2026-03-20 23:12:46 +0530

    Using Sorting and Binary Search - O(n Log n) time and O(1) space

--- Data_structure_Practice/Sorting/Pair_diff.java
+++ Data_structure_Practice/Sorting/Pair_diff.java
@@ -1,5 +1,7 @@
 package Data_structure_Practice.Sorting;
 
+import java.util.Arrays;
+
 public class Pair_diff {
 
     //[Naive Approach] Using 2 Nested Loops
@@ -18,11 +20,48 @@ public class Pair_diff {
         return false;
     }
 
+    //Using Sorting and Binary Search
+    //Time Complexity :- O(n log(n)) & Space Complexity : O(1)     
+    public static boolean Find_pair(int[] arr, int x){
+        Arrays.sort(arr);
+
+        int n = arr.length;
+
+        for(int i = 0; i < n; i++){
+            int target = arr[i] + x;
+
+            if (binarySearch(arr, i + 1, n - 1, target)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    // Binary search function
+    public static boolean binarySearch(int[] arr, int left, int right, int key){
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+
+            if (arr[mid] == key) {
+                return true;
+            } else if (arr[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+
     public static void main(String[] args) {
         int[] arr = { 5, 20, 3, 2, 50, 80 };
         int x = 78;
 
-        if (findPair(arr, x)) {
+        if (Find_pair(arr, x)) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
