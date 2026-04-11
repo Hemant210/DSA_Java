e28e72c24263341e3d0f5fd653b4b18f658e74f1
Hemant210 <hemantpatil.2103@gmail.com>
2026-04-11 22:56:23 +0530

    Update Time Complexity

--- Data_structure_Practice/Sorting/counting_sort.java
+++ Data_structure_Practice/Sorting/counting_sort.java
@@ -2,45 +2,45 @@ package Data_structure_Practice.Sorting;
 
 public class counting_sort {
     // Time Complexity :- O(n + k) & Space Complexity : O(n + k)
-    // public static int[] countsort(int[] arr) {
-    // if (arr.length == 0) {
-    // return new int[0];
-    // }
+    public static int[] countsort(int[] arr) {
+        if (arr.length == 0) {
+            return new int[0];
+        }
 
-    // int n = arr.length;
+        int n = arr.length;
 
-    // // Find Maximum Value
-    // int maxVal = arr[0];
+        // Find Maximum Value
+        int maxVal = arr[0];
 
-    // for (int i = 1; i < n; i++) {
-    // if (arr[i] > maxVal) {
-    // maxVal = arr[i];
-    // }
-    // }
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
 
-    // // Create Count array
-    // int[] cntArr = new int[maxVal + 1];
+        // Create Count array
+        int[] cntArr = new int[maxVal + 1];
 
-    // // Count frequency of each element
-    // for (int i = 0; i < n; i++) {
-    // cntArr[arr[i]]++;
-    // }
+        // Count frequency of each element
+        for (int i = 0; i < n; i++) {
+            cntArr[arr[i]]++;
+        }
 
-    // // Compute prefix sum
-    // for (int i = 1; i <= maxVal; i++) {
-    // cntArr[i] += cntArr[i - 1];
-    // }
+        // Compute prefix sum
+        for (int i = 1; i <= maxVal; i++) {
+            cntArr[i] += cntArr[i - 1];
+        }
 
-    // // Build answer array
-    // int[] ans = new int[n];
-    // for (int i = n - 1; i >= 0; i--) {
-    // int v = arr[i];
-    // ans[cntArr[v] - 1] = v;
-    // cntArr[v]--;
-    // }
+        // Build answer array
+        int[] ans = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            int v = arr[i];
+            ans[cntArr[v] - 1] = v;
+            cntArr[v]--;
+        }
 
-    // return ans;
-    // }
+        return ans;
+    }
 
     public static void CountSort(int[] nums) {
         int maxVal = nums[0];
