44c5f1d7bd05b8901afa6d5dbb38ccd954db1e4b
Hemant210 <hemantpatil.2103@gmail.com>
2026-05-23 21:04:01 +0530

    Update Time Complexity

--- Data_structure_Practice/Sorting/Ceiling_element.java
+++ Data_structure_Practice/Sorting/Ceiling_element.java
@@ -21,7 +21,7 @@ public class Ceiling_element {
         return -1;
     }
     
-    // Binary Search Approch
+    // Binary Search Approch Better
     // Time Complexity: O(Log n) & Space Complexity: O(1)
     public static int ceil_search(int[] arr, int x){
         int start = 0, end = arr.length - 1, res = -1;
