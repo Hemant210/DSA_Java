2612d75a564c90b5570e49c74c9952cb018cd36d
Hemant210 <hemantpatil.2103@gmail.com>
2026-05-27 21:05:05 +0530

    Update Time Complexity

--- Data_structure_Practice/Binary_Search/Minimized_maximum.java
+++ Data_structure_Practice/Binary_Search/Minimized_maximum.java
@@ -1,9 +1,7 @@
 package Data_structure_Practice.Binary_Search;
 
 public class Minimized_maximum {
-    public void mini(int a){
-        
-    }
+    
     public static void main(String[] args) {
         
     }
