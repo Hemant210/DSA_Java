0291259dbcda4a7671ed71c26d7292b6b5fefbe0
Hemant210 <hemantpatil.2103@gmail.com>
2026-06-26 23:18:09 +0530

    Update code

--- Data_structure_Practice/Sorting/Sort_Colors.java
+++ Data_structure_Practice/Sorting/Sort_Colors.java
@@ -60,7 +60,7 @@ public class Sort_Colors {
     public static void main(String[] args) {
         int[] nums = { 2, 0, 2, 1, 1, 0 };
 
-        //sortColors(nums);
+        sortColors(nums);
 
         sort_color(nums);
 
