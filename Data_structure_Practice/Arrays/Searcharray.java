86d7aa180e9fbaa2a690b32bdc2d7313b511a4f4
Hemant210 <hemantpatil.2103@gmail.com>
2026-07-01 23:54:09 +0530

    Update code

--- Data_structure_Practice/Arrays/Searcharray.java
+++ Data_structure_Practice/Arrays/Searcharray.java
@@ -64,7 +64,7 @@ public class Searcharray {
         int[] arr = { -1, 0, 3, 5, 9, 12 };
         int target = 0;
         //System.out.println(Find_rotedarray(arr, 9));
-        //System.out.println(Find_Element(arr, 12));
+        System.out.println(Find_Element(arr, 12));
         System.out.println(Recursion_binarysearch(arr, target, 0, arr.length - 1));
     }
 }
